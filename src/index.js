5c81b6c82f812c3795e0d8c444c4840f6d60febb
Jake Felice <jasonfelice0@gmail.com>
2022-09-28 00:20:11 -0700

    Update api link

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { Provider } from 'react-redux';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import store from './redux/configureStore';
 
--- src/redux/message.js
+++ src/redux/message.js
@@ -4,7 +4,7 @@ const getMessage = (payload) => ({ type: 'GET_MESSAGE', payload });
 const initialState = [];
 
 const fetchMessage = () => async (dispatch) => {
-  const res = await axios.get('http://127.0.0.1:3000/api/v1/messages');
+  const res = await axios.get('http://localhost:3000/api/v1/messages');
   dispatch(getMessage(res.data));
 };
 
